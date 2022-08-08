8ac5777a8ae0cf1fa77499be9abbc13e5a60323d
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2022-08-08 10:55:13 +0200

    Add assertion which does not have the problem

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -4,7 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExampleTest {
     @Test
-    void test() {
+    void test_reports_timeout_incorrectly() {
         assertThat(Example.answer()).isEqualTo(21);
+    }
+    
+    @Test
+    void test_correct_message() {
+        assertThat(new int[]{Example.answer()}).contains(21);
     }
 }
